0d146e9d1239df1e7431dbf81bfa0ba9f3645ae8
Manas Mishra <manasmishra2520@gmail.com>
2022-09-03 18:44:03 +0530

    improved authentication

--- middleware.js
+++ middleware.js
@@ -1,6 +1,10 @@
 // creating a middleware function to check if a user is loggedIn or not
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        // storing the original url the user meant to access despite not being loggedIn. 
+        // Once the user logs in, redirect to the path where user was previously trying to go instead of redirecting to campgrounds
+        // here, "returnTo" is an arbitrary name being stored as key in the session. You could have named it "bhrooshwasoo" or "stupidUser".
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must be loggedIn to perform this action');
         // adding return is preferred if using middleware
         return res.redirect('/login');
--- routes/users.js
+++ routes/users.js
@@ -15,9 +15,13 @@ router.post('/register', catchAsync(async (req, res) => {
         const user = new User({ email, username });
         // we pass on the password while registering the user, all this is because of passport.js, that is how it works
         const registeredUser = await User.register(user, password);
-        console.log(registeredUser);
-        req.flash('success', 'Welcome to Yelp Camp!');
-        res.redirect('/campgrounds');
+        // when a user registers, automatically log him in using req.login method provided by passport
+        req.login(registeredUser, err => {
+            // weird syntax that you just have to deal with
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
+        });
     }
     catch (e) {
         req.flash('error', e.message);
@@ -32,7 +36,12 @@ router.get('/login', (req, res) => {
 // here we are using a middleware provided to us by passport.js
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', `Welcome back ${req.user.username}!`);
-    res.redirect('/campgrounds');
+    // checking if the user was trying to access any url before logging In.
+    // if the user wasn't trying to go anywhere and was simply logging In, redirect to the default "/campgrounds" page
+    // FIXME : Fix the returnTo problem
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 });
 
 // logout route
